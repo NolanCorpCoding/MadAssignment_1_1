b9c6e94ee8c860f940716c9faf6ec6baff64bd0b
tobeymartin <20568164@student.curtin.edu.au>
2022-09-19 23:51:51 +0800

    Made DB Schema for Cart, Meal and Restaurant Tables within the DB

---
+++ app/src/main/java/com/example/madassignment_1_1/Cart/CartDBSchema.java
@@ -0,0 +1,15 @@
+package com.example.madassignment_1_1.Cart;
+
+public class CartDBSchema
+{
+    public static class CartTable{
+        public static final String NAME = "Cart";
+        public static class Cols{
+            public static final String ID = "id";
+            public static final String FOODITEMLIST = "foodItemList";
+            public static final String PRICE = "price";
+            public static final String USERACCOUNTID = "userAccountID";
+            public static final String RESTAURANTID = "restaurantID";
+        }
+    }
+}
---
+++ app/src/main/java/com/example/madassignment_1_1/Restaurants/RestaurantDBSchema.java
@@ -0,0 +1,14 @@
+package com.example.madassignment_1_1.Restaurants;
+
+public class RestaurantDBSchema
+{
+    public static class RestaurantTable{
+        public static final String NAME = "Restaurant";
+        public static class Cols{
+            public static final String ID = "id";
+            public static final String NAME = "name";
+            public static final String ADDRESS = "address";
+            public static final String DRAWABLEREFERENCE = "drawableReference";
+        }
+    }
+}
