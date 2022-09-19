c66aa3441b846000b85be1c50d2661f48c125db8
tobeymartin <20568164@student.curtin.edu.au>
2022-09-20 00:06:31 +0800

    Created the DB helper for the User Account Database Table
    Need to figure out how to link stuff and work through this without being able to store lists and instances of objects

---
+++ app/src/main/java/com/example/madassignment_1_1/Account/UserAccountDBHelper.java
@@ -0,0 +1,28 @@
+package com.example.madassignment_1_1.Account;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import com.example.madassignment_1_1.Account.UserAccountDBSchema.UserAccountTable;
+
+
+public class UserAccountDBHelper extends SQLiteOpenHelper
+{
+    private static final int VERSION = 1;
+    private static final String DATABASE_NAME = "UserAccount.db";
+
+    public UserAccountDBHelper(Context context) {
+        super(context, DATABASE_NAME, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase)
+    {
+        sqLiteDatabase.execSQL("create table "+UserAccountTable.NAME+"("+UserAccountTable.Cols.ID+" INT, "+UserAccountTable.Cols.FIRSTNAME+" TEXT,  "+UserAccountTable.Cols.LASTNAME+" TEXT,  "+UserAccountTable.Cols.EMAIL+" TEXT,  "+UserAccountTable.Cols.CURRENTCART+" TEXT, "+ UserAccountTable.Cols.PREVIOUSCARTS+ " INT);");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
