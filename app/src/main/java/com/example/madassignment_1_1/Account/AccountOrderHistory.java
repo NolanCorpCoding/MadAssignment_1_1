0f119704c9ca32be4eda8c9048c43415c78fa70a
Sam <lasersharks1@gmail.com>
2022-09-23 23:56:59 +0800

    added some order history stuff

--- app/src/main/java/com/example/madassignment_1_1/Account/AccountOrderHistory.java
+++ app/src/main/java/com/example/madassignment_1_1/Account/AccountOrderHistory.java
@@ -2,13 +2,25 @@ package com.example.madassignment_1_1.Account;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.madassignment_1_1.Meals.MealsFrag;
 import com.example.madassignment_1_1.R;
+import com.example.madassignment_1_1.Restaurants.Restaurant;
+import com.example.madassignment_1_1.Restaurants.RestaurantList;
+import com.example.madassignment_1_1.Restaurants.RestaurantsFrag;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,6 +37,9 @@ public class AccountOrderHistory extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private AccountOrderHistory thisFrag = this;
+    private FragmentManager fm = getParentFragmentManager();
 
     public AccountOrderHistory() {
         // Required empty public constructor
@@ -61,6 +76,78 @@ public class AccountOrderHistory extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account_order_history, container, false);
+        //return inflater.inflate(R.layout.fragment_restaurants, container, false);
+        View view = inflater.inflate(R.layout.fragment_account_order_history, container, false);
+        RecyclerView rv = view.findViewById(R.id.rvOrders);
+        rv.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
+        RestaurantsFrag.RestaurantAdapter adapter = new RestaurantsFrag.RestaurantAdapter(resList);
+        rv.setAdapter(adapter);
+        return view;
+    }
+
+
+
+
+
+
+
+    public class OrderViewHolder extends RecyclerView.ViewHolder {
+        TextView textName;
+        TextView textAddress;
+        ImageView img;
+
+        public OrderViewHolder(@NonNull View itemView, ViewGroup parent) {
+            super(itemView);
+            textName = itemView.findViewById(R.id.resName);
+            textAddress = itemView.findViewById(R.id.resAddr);
+            img = itemView.findViewById(R.id.imgResIcon);
+
+        }
+    }
+
+
+
+
+
+    public class OrderAdapter extends RecyclerView.Adapter<AccountOrderHistory.OrderViewHolder> {
+
+        RestaurantList data;
+
+        public OrderAdapter(RestaurantList data){
+            this.data = data;
+        }
+
+        @NonNull
+        @Override
+        public AccountOrderHistory.OrderViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            Log.d("adapter: ", "on create view");
+            LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+            View view = layoutInflater.inflate(R.layout.orderhistoryentry,parent,false);
+            AccountOrderHistory.OrderViewHolder myViewHolder = new AccountOrderHistory.OrderViewHolder(view, parent);
+            return myViewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull AccountOrderHistory.OrderViewHolder holder, int position) {
+            Restaurant restaurant = resList.get(position);
+            holder.img.setImageResource(restaurant.getDrawableID());
+            holder.textName.setText(restaurant.getName());
+            holder.textAddress.setText(restaurant.getAddress());
+
+            holder.img.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(view.getContext(), "clicked menu item", Toast.LENGTH_SHORT).show();
+                    //THIS IS WHERE THE "ACTION" HAPPENS
+                    fm.beginTransaction().remove(thisFrag).commit();
+                    fm.beginTransaction().add(R.id.frameLayout, new MealsFrag()).commit();
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return data.size();
+        }
     }
 }
